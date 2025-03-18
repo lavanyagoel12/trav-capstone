7e491154c64dafa96780a0f96e52fa7ae1afd26b
Joewid Sharza <joewidsharza@gmail.com>
2025-03-18 14:19:08 -0400

    React added

--- server/random_data.py
+++ server/random_data.py
@@ -10,10 +10,23 @@ def generate_random_name():
     last_names = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez"]
     return f"{random.choice(first_names)} {random.choice(last_names)}"
 
+# Record data --NOT FINAL-- 
+def generate_record_name():
+    first_word = ["empty", "full", "available", "unavailable", "valid", "invalid", "ready", "busy",
+    "new", "old", "small", "large", "high", "low", "first", "last", "next", "previous",
+    "active", "inactive", "public", "private", "open", "closed", "visible", "hidden",
+    "true", "false", "safe", "unsafe", "clean", "dirty", "fast", "slow", "strong", "weak",
+    "online", "offline", "local", "remote", "default", "custom", "simple", "complex",
+    "important", "trivial", "critical", "normal", "urgent", "pending", "running", 
+    "finished", "failed", "successful"]
+    last_word = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez"]
+    return f"{random.choice(first_word)} {random.choice(last_word)}"
+
 def generate_customer_data(num_data_points=1000):
     data = []
     for _ in range(num_data_points):
         customer_name = generate_random_name()
+        record_name = generate_record_name()
         popularity = random.randint(1, 5)
         condition = random.randint(1, 10)
         price = round(random.uniform(3.99, 30.99), 2)
@@ -21,6 +34,7 @@ def generate_customer_data(num_data_points=1000):
 
         data_point = {
             "customer_name": customer_name,
+            "record_name": record_name,
             "popularity": popularity,
             "condition": condition,
             "price": price,
